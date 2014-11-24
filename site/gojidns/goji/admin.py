b1ccdf242be2954c916eb4d8fb3347fd0cc73164
Durand John Miller <durand.miller@smksoftware.com>
2014-11-24 22:04:30 +0200

    Remove a mistaken line

--- site/gojidns/goji/admin.py
+++ site/gojidns/goji/admin.py
@@ -14,8 +14,6 @@
 
 # You should have received a copy of the GNU General Public License
 # along with GojiDNS.  If not, see <http://www.gnu.org/licenses/>.
-# from django.conf.urls import patterns, include, url
-#
 
 
 from django.contrib import admin
